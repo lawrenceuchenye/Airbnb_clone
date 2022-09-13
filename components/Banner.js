ce9b39a987c9d526aa9b8e13f70dff664edc321c
lawrence_k_uchenye <lawuche29@gmail.com>
2022-09-13 10:36:52 +0100

    fixing stuff

--- components/Banner.js
+++ components/Banner.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const Banner=()=>{
     return(
-        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px]  w-screen">
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px]  w-auto">
             <Image src="https://links.papareact.com/0fm" layout="fill" objectFit="cover" objectPosition="center"/>
              <div className="absolute top-1/2 w-full text-center">
                 <p className="text-1xl font-bold">Not sure where to go?,Perfect.</p>
